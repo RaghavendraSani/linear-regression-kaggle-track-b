4d303d3e31bf9563496eac8aa0e21d59d8894aa4
RaghavendraSani <raghavendrasinghsani@gmail.com>
2026-06-10 17:15:48 +0530

    GD_v1

--- src/grading_decent.py
+++ src/grading_decent.py
@@ -6,55 +6,34 @@ train = pd.read_csv("../data/train.csv")
 X = train.drop(columns=["Id", "Target"]).values
 y = train["Target"].values
 
-print("x shape: ",X.shape)
-print("y shape: ",y.shape)
-
 n_features = X.shape[1]
 
 weights = np.zeros(n_features)
 bias = 0.0
 
-print("weights: ",weights)
-print("bias: ",bias)
-
 y_pred = X @ weights + bias
-print("First 10 predictions: ",y_pred[:10])
 
 error = y_pred - y
-sq_error = np.square(error)
-mse = np.mean(sq_error)
-
-print("Mean squared error: ",mse)
+mse = np.mean(error**2)
 
 learning_rate = 0.000001
 epochs = 1000
 
-print ("epochs: ",epochs)
-print("Learning rate: ",learning_rate)
-
 n = len(y)
-dw = (2/n) * np.dot(X.T, (y_pred - y))
-db = (2/n) * np.sum(y_pred - y)
 
-print("dw: ",dw)
-print("db: ",db)
+for epoch in range(epochs):
+    y_pred = X @ weights + bias
+    error = y_pred - y
+    mse = np.mean(error**2)
 
-print("weights before: ",weights)
-print("bias brefore: ",bias)
+    dw = (2/n) * np.dot(X.T, (y_pred - y))
+    db = (2/n) * np.sum(y_pred - y)
 
-weights = weights - learning_rate * dw
-bias = bias - learning_rate * db
+    weights = weights - learning_rate * dw
+    bias = bias - learning_rate * db
 
-print("weights after: ",weights)
-print("bias after: ",bias)
+    if (epoch+1)%100 == 0:
+        print(f"Epoch: {epoch+1}, mse: {mse}")
 
-y_new_pred = X @ weights + bias
-new_error = y_new_pred - y
-new_sq_error = np.square(new_error)
-new_mse = np.mean(new_sq_error)
-
-print("old mse: ",mse)
-print("new mse: ",new_mse)
-
-
-
+print("final weights: ", weights)
+print("final bias: ", bias)
